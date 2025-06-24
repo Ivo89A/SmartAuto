d2496c39d904c54e1f1d389763454d3f0ac9f1a8
Ivo89A <ivoalaniz671@gmail.com>
2025-06-24 13:11:23 -0300

    Implementacion de base de datos con Supabase

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import { createChat } from 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js';
 
-function enviarFormulario(event) {
-    event.preventDefault();
 
-    const nombre = document.getElementById('nombre').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const mensaje = document.getElementById('mensaje').value.trim();
 
-    if (!nombre || !email || !mensaje) {
-        alert('Por favor completa todos los campos.');
-        return;
-    }
+//Envio de datos a Supabase
+
+
+// app.js
+const supabase = supabase.createClient('https://xpgaxmooupfgmtkfzciu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwZ2F4bW9vdXBmZ210a2Z6Y2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3ODAwMzgsImV4cCI6MjA2NjM1NjAzOH0.2juPb6KxNZv4nV03Pkl8Q4mnpYWveH4CPGNXr2YVcBs');
+
+async function enviarFormulario(event) {
+  event.preventDefault();
+  const nombre = document.getElementById('nombre').value;
+  const correo = document.getElementById('correo').value;
+  const mensaje = document.getElementById('mensaje').value;
+
+  const { error } = await supabase.from('contactos').insert([{ nombre, correo, mensaje }]);
+
+  if (error) {
+    alert('Hubo un error al enviar.');
+    console.error(error);
+  } else {
+    alert('¡Mensaje enviado con éxito!');
+  }
+}
+
+
+  guardarContacto();
 
     // Enviar datos al webhook/backend
     fetch('https://n8n-production-5982.up.railway.app/webhook/lead-lost-webhook', {
@@ -31,4 +46,4 @@ function enviarFormulario(event) {
     .catch(() => {
         alert('No se pudo enviar el mensaje. Verifica tu conexión o intenta más tarde.');
     });
-}
+
